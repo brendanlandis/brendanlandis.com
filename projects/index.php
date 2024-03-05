7d32be19309ac3012569bb1bec817e2c688a6ee8
Brendan Landis <b@slownames.net>
2024-03-05 14:59:46 -0500

    adding projects template

---
+++ projects/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Brendan Landis : Projects</title>
+        <base href="https://brendanlandis.com/" />
+        <?php include 'html-header.php'; ?>
+    </head>
+
+    <body class="about">
+        <?php include "header.php"; ?>
+        <main>
+            <p>Projects</p>
+        </main>
+        <?php include "footer.php"; ?>
+    </body>
+</html>
