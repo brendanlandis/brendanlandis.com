63ea63c0963dddd3188e950a28f25459d4cae7d2
Brendan Landis <b@slownames.net>
2024-03-05 18:26:09 -0500

    adding resume content

--- resume/index.php
+++ resume/index.php
@@ -1,18 +1,134 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Brendan Landis : Resume</title>
-        <base href="/" />
-        <?php include '../html-header.php'; ?>
-    </head>
 
-    <body class="resume">
-        <?php include "../header.php"; ?>
-        <main>
-            <h2>Resume</h2>
-        </main>
-        <?php include "../footer.php"; ?>
-    </body>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Brendan Landis : Resume</title>
+    <base href="/" />
+    <?php include '../html-header.php'; ?>
+</head>
+
+<body class="resume">
+    <?php include "../header.php"; ?>
+    <main>
+        <h2>Resume</h2>
+        <section id="experience">
+            <h3>Experience</h3>
+            <ul>
+                <li>
+                    <h4>
+                        <span class="job-title">Frontend Developer</span>
+                        <span class="job-company">Electro / The Clorox Company</span>
+                    </h4>
+                    <div class="job-duration">Oakland, CA &mdash; 2013-2023</div>
+                    <ul class="job-notes">
+                        <li>Responsible for turning intricate wireframes into modular and scalable code using WCAG-compliant HTML, CSS / Sass, and Javascript.</li>
+                        <li>Crafted and documented base styles and templates for numerous high-profile projects (clorox.com, pinesol.com, glad.com, kingsford.com, liquidplumr.com, and more).</li>
+                        <li>Responsible for critical bug fixes on live code.</li>
+                        <li>Advised teams on web accessibility standards.</li>
+                        <li>Expert level knowledge of WordPress templating systems (Twig / Timber, Advanced Custom Fields, Genesis Blocks).</li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>
+                        <span class="job-title">Lead Frontend Developer</span>
+                        <span class="job-company">GatheringPoint</span>
+                    </h4>
+                    <div class="job-duration">San Francisco, CA &mdash; 2010-2013</div>
+                    <ul class="job-notes">
+                        <li>Designed and developed a responsive, map-based web app that aggregated and interpreted location-based data from many APIs, ranging from information on local pollution levels to location-enabled tweets.</li>
+                        <li>Helped facilitate deployment of the app as an iframe-based service to third party sites.</li>
+                        <li>Worked with core team of developers on numerous client projects, both internal and public-facing.</li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>
+                        <span class="job-title">Frontend Developer &amp; Sound Designer</span>
+                        <span class="job-company">Moblyng Inc.</span>
+                    </h4>
+                    <div class="job-duration">Redwood City, CA &mdash; 2009-2010</div>
+                    <ul class="job-notes">
+                        <li>Designed and developed interfaces for phone games that were published across multiple desktop and mobile platforms (Facebook, iPhone, Android, Nokia, Palm).</li>
+                        <li>Composed music and sound effects for games.</li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>
+                        <span class="job-title">Web Director</span>
+                        <span class="job-company">Berklee Internet Radio Network</span>
+                    </h4>
+                    <div class="job-duration">Boston, MA &mdash; 2007-2008</div>
+                    <ul class="job-notes">
+                        <li>Designed, developed, and helped launch a web site for the newly-launched internet radio station at Berklee College of Music.</li>
+                        <li>Delivered presentations on how to operate WordPress to groups of 50-100 music students.</li>
+                        <li>Tutored small team of students on standards-based, accessibility-focused web development and basic design principles.</li>
+                        <li>Managed a site redesign with a small team using Ruby on Rails. Prepared the site for deployment to Berklee's fifteen international schools.</li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>
+                        <span class="job-title">Client Side Developer</span>
+                        <span class="job-company">Publicis Dialog</span>
+                    </h4>
+                    <div class="job-duration">San Francisco, CA &mdash; 2006</div>
+                    <ul class="job-notes">
+                        <li>Designed and developed user interfaces for custom business applications, as well as developed promotional emails for opt-in mailing lists. Clients included Sprint, Hewlett-Packard, WebEx, and others.</li>
+                        <li>Performed occasional project management involving coordinating workflow between departments.</li>
+                        <li>Participated in revision of business processes and best practices.</li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>
+                        <span class="job-title">Designer / Developer</span>
+                        <span class="job-company">Pop Interactive</span>
+                    </h4>
+                    <div class="job-duration">San Francisco, CA &mdash; 2004-2005</div>
+                    <ul class="job-notes">
+                        <li>Worked with custom package system developing hand-coded sites for businesses in the US and UK.</li>
+                        <li>Gave technical support to clients regarding email and general computer issues.</li>
+                        <li>Trained new employees in standards-compliant web development and fielded technical questions regarding CSS.</li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>
+                        <span class="job-title">Designer / Developer</span>
+                        <span class="job-company">RayGun Design</span>
+                    </h4>
+                    <div class="job-duration">Mill Valley, CA &mdash; 2002-2004</div>
+                    <ul class="job-notes">
+                        <li>Designed and developed web sites for local businesses, musicians, and artists.</li>
+                        <li>Consulted individuals and small businesses on small business workflow, project management, personal and office organizational systems, computer literacy / networking / repair.</li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+        <section id="skills">
+            <h3>Skills</h3>
+            <ul>
+                <li>Frontend design and development using HTML, modular and scalable CSS / Sass, and Javascript, with a fondness for a11y and user & developer experience.</li>
+                <li>Flexibility regarding the ever-changing ecosystem(s) of frontend technologies, with an understanding that as sites grow and mature, so too do their technological needs.</li>
+                <li>Presently focused on building my React and vanilla Javascript skills.</li>
+                <li>Expert level knowledge of Adobe Photoshop. Intermediate level knowledge of InDesign and Illustrator.</li>
+                <li>Fluency with version control software (Git, SVN, et cetera).</li>
+                <li>Generally quick to learn and don't mind being out of my element. Appreciative of new challenges & plays well with others.</li>
+            </ul>
+        </section>
+        <section id="education">
+            <h3>Education</h3>
+            <ul>
+                <li>
+                    <h4>Self-Instruction, 1998-present</h4>
+                    <p>I fell into computers at age 14, and I've been learning and growing as a designer and developer since then &mdash; over half my life.</p>
+                </li>
+            </ul>
+        </section>
+        <section id="references">
+            <h3>References</h3>
+            <p>References available upon request.</p>
+        </section>
+    </main>
+    <?php include "../footer.php"; ?>
+</body>
+
 </html>
