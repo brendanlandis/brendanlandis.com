f5162ec55866132c0aeb9cdf25deb259e3635bdd
Brendan Landis <b@slownames.net>
2024-04-21 17:35:03 -0400

    let's try moving everything

---
+++ public/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'slownames/brendan-landis',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '48ea306fc6e7047111cb74c225f14bb2eb16ad75',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'slownames/brendan-landis' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '48ea306fc6e7047111cb74c225f14bb2eb16ad75',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
