1dc8390fe92452bab561d27a425c3989410f624c
Brendan Landis <b@slownames.net>
2024-04-22 08:56:14 -0400

    copy change

--- header.php
+++ header.php
@@ -1,20 +1,8 @@
 <header>
     <h1>Brendan Landis</h1>
     <div id="sub-h1">designer &amp; front-end developer</div>
-    <nav
-        class="navbar navbar-expand-lg"
-        role="navigation"
-        aria-label="Main"
-    >
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#main-nav-container"
-            aria-controls="main-nav-container"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
+    <nav class="navbar navbar-expand-lg" role="navigation" aria-label="Main">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav-container" aria-controls="main-nav-container" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="main-nav-container">
@@ -32,34 +20,22 @@
             </ul>
             <ul id="social">
                 <li>
-                    <a
-                        href="https://www.instagram.com/slow.names/"
-                        title="see me mostly post about my bands on instagram"
-                    >
+                    <a href="https://www.instagram.com/slow.names/" title="see me post mostly about my bands on instagram">
                         <span class="icon icon-instagram"></span>
                     </a>
                 </li>
                 <li>
-                    <a
-                        href="https://github.com/brendanlandis"
-                        title="check out my code on github"
-                    >
+                    <a href="https://github.com/brendanlandis" title="check out my code on github">
                         <span class="icon icon-github"></span>
                     </a>
                 </li>
                 <li>
-                    <a
-                        href="https://mstdn.social/@slownames"
-                        title="read my very occasional posts on mastodon"
-                    >
+                    <a href="https://mstdn.social/@slownames" title="read my very occasional posts on mastodon">
                         <span class="icon icon-mastodon"></span>
                     </a>
                 </li>
                 <li>
-                    <a
-                        href="&#x6D;a&#105;&#x6C;t&#111;&colon;b&#64;&#115;lo&#x77;&#110;am&#101;s&period;&#110;e&#x74;"
-                        title="email me"
-                    >
+                    <a href="&#x6D;a&#105;&#x6C;t&#111;&colon;b&#64;&#115;lo&#x77;&#110;am&#101;s&period;&#110;e&#x74;" title="email me">
                         <span class="icon icon-email"></span>
                     </a>
                 </li>
