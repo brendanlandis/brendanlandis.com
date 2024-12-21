105cbe91f4c08e4012da95a8857c2a82bfcb1800
Brendan Landis <b@slownames.net>
2024-12-21 08:40:58 -0500

    layout tweaks

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -6,7 +6,7 @@ export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme} className="themeToggle">
+    <button onClick={toggleTheme} id="themeToggle">
       {theme === 'light' ? <MoonStars size={40} weight="thin" /> : <SunHorizon size={40} weight="thin" />}
     </button>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from 'next/font/local';
 import './css/screen.css';
 import Image from 'next/image';
 import copperImage from './img/copper.jpg';
-import { Yarn } from '@phosphor-icons/react/dist/ssr';
+import { Yarn, Stairs, PottedPlant, Heartbeat, HandHeart, Graph, FlyingSaucer } from '@phosphor-icons/react/dist/ssr';
 import ThemeToggle from './components/ThemeToggle';
 
 export const metadata: Metadata = {
@@ -37,8 +37,15 @@ export default function RootLayout({
           <Image src={copperImage} alt="" width={2048} height={2048} />
         </div>
         <header>
+          <div></div>
           <div id="headerIcon">
-            <Yarn size={80} weight="thin" />
+            {/* <Yarn size={80} weight="thin" /> */}
+            {/* <Stairs size={80} weight="thin" /> */}
+            {/* <PottedPlant size={80} weight="thin" /> */}
+            {/* <Heartbeat size={80} weight="thin" /> */}
+            {/* <HandHeart size={80} weight="thin" /> */}
+            {/* <Graph size={80} weight="thin" /> */}
+            <FlyingSaucer size={80} weight="thin" />
           </div>
           <ThemeToggle />
         </header>
